8b1c0023ac025b034753c525681137e525f68a01
DTSmith17 <153142161+DTSmith17@users.noreply.github.com>
2024-04-02 17:43:16 -0500

    added sequelize and required .env authentication

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+const sequelize = require("./config/connection");
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'employeetracker_db'
-    },
-    console.log(`Connected to the employee tracker database.`)
-  );
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  app.use((req, res) => {
-    res.status(404).end();
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});

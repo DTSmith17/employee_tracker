c9084dea4c7df1e5d27e1d493f18e9fe704c388c
DTSmith17 <153142161+DTSmith17@users.noreply.github.com>
2024-04-15 15:25:17 -0500

    function  to update employee roles

--- db/test.sql
+++ db/test.sql
@@ -1,4 +1,4 @@
 -- Active: 1710293330467@@127.0.0.1@3306@employeetracker_db
-select * from departments; 
-select * from roles; 
+select * from department; 
+select * from role; 
 select * from employee;
